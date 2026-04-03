983893f6e649b86dd29c7d7f3386c0f8e006fe5f
Gabriel Iwakura <gabriel.iwakura@sptech.school>
2026-04-03 14:59:22 -0300

    Alterando o yaml pra suportar imagens maiores

--- src/main/java/lotus/flor/arquivo/UploadApplication.java
+++ src/main/java/lotus/flor/arquivo/UploadApplication.java
@@ -2,9 +2,11 @@ package lotus.flor.arquivo;
 
 import lotus.flor.arquivo.application.usecase.UploadFileUseCase;
 import lotus.flor.arquivo.infrastructure.aws.S3StorageAdapter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
 
 @SpringBootApplication
@@ -15,14 +17,17 @@ public class UploadApplication {
 	}
 
 	@Bean
-	public S3AsyncClient s3AsyncClient() {
-		return S3AsyncClient.builder().build();
+	public S3AsyncClient s3AsyncClient(@Value("${aws.s3.region}") String region) {
+		return S3AsyncClient.builder()
+				.region(Region.of(region))
+				.build();
 	}
 
 	@Bean
-	public UploadFileUseCase uploadFileUseCase(S3AsyncClient client) {
+	public UploadFileUseCase uploadFileUseCase(S3AsyncClient client,
+			@Value("${aws.s3.bucket}") String bucket) {
 		return new UploadFileUseCase(
-				new S3StorageAdapter(client, "meu-bucket")
+				new S3StorageAdapter(client, bucket)
 		);
 	}
 }
