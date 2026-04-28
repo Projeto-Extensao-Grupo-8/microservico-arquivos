9e7a9e9094cd0592920ffeedea0621a70efc0d22
Gabriel Iwakura <gabriel.iwakura@sptech.school>
2026-04-28 03:34:45 -0300

    fix: s3 config

--- src/main/java/lotus/flor/arquivo/infrastructure/config/S3Config.java
+++ src/main/java/lotus/flor/arquivo/infrastructure/config/S3Config.java
@@ -3,7 +3,7 @@ package lotus.flor.arquivo.infrastructure.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
 
@@ -17,7 +17,7 @@ public class S3Config {
     public S3AsyncClient s3AsyncClient() {
         return S3AsyncClient.builder()
                 .region(Region.of(region))
-                .credentialsProvider(InstanceProfileCredentialsProvider.create())
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
